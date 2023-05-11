838e9268f443f17c4ff82a94c1300d34c6660689
jfmemon <jannatul15-2497@diu.edu.bd>
2023-05-12 02:42:32 +0600

    added custom route

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
-
 const categories = require('./Data/Categories.json');
+const news = require('./Data/news.json');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello world.');
+    res.send('Server is running.');
 })
 
 app.get('/news-category', (req, res) => {
     res.send(categories);
 })
 
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == "8") {   // url a jodi id er jaygay 08 thake, tahole
+        res.send(news);
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === id);
+        res.send(selectedCategory);
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
 app.listen(port, () => {
     console.log('Latest news server is running on:', port);
 })
-
